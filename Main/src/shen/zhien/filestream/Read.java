dfdf1c45f00b71082ce3b025c83801d925a98c47
zhienshen <huan18971374646@163.com>
2025-07-20 18:52:07 +0800

    Update

--- Main/src/shen/zhien/filestream/Read.java
+++ Main/src/shen/zhien/filestream/Read.java
@@ -14,21 +14,8 @@ public final class Read {
      * @param setting The reading settings. Use the settings defined in the {@link Settings} class to specify how the file should be read.Please use '|' to combine multiple settings into one setting.Such as:<code>{@link Settings#out} | {@link Settings#binary}</code>
      * @return A string containing the content read from the file, or null if the file does not exist or the file is null or when reading file, it has something wrong.
      * @see Settings
-     * @see #ReadFile(String, char, int)
      */
-    public static native String ReadFile(String File, int setting);
-
-    /**
-     * Reads content from a file.Use custom delimiter.
-     *
-     * @param File    The file path from which the content will be read.
-     * @param delim   The delimiter used to separate content in the file. This can be any character that is used to split the content into parts.
-     * @param setting The reading settings. Use the settings defined in the {@link Settings} class to specify how the file should be read.Please use '|' to combine multiple settings into one setting.Such as:<code>{@link Settings#out} | {@link Settings#binary}</code>
-     * @return A string containing the content read from the file, or null if an error occurs.
-     * @see Settings
-     * @see #ReadFile(String, int)
-     */
-    public static native String ReadFile(String File, char delim, int setting);
+    public static native String ReadFile(String File, int lines, int setting);
 
     /**
      * Read the all content of the file.
---
+++ Main/src/shen/zhien/filestream/package-info.java
@@ -0,0 +1,11 @@
+/**
+  This package provides classes and methods for file stream operations, including reading and writing files.
+  <p>
+  The main classes in this package are:
+  <ul>
+      <li>{@link shen.zhien.filestream.Read} - For reading content from files.</li>
+      <li>{@link shen.zhien.filestream.Write} - For writing content to files.</li>
+      <li>{@link shen.zhien.filestream.Settings} - For defining settings used in file operations.</li>
+  </ul>
+ */
+package shen.zhien.filestream;
